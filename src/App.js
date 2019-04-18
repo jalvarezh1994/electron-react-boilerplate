5603b6787cf4d4e7cdbf1ae30a9f5b4725799b79
jalvarezh1994 <j.alvarezh1994@unitec.edu>
2019-04-18 04:31:03 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    console.log('holis');
+  }
+  render() {
+    return (
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat omnis expedita pariatur quasi et, quaerat tempore provident, consequatur quae tenetur ex at nihil itaque minima reiciendis animi quod! Quibusdam, nihil!
+      </div>
+    );
+  }
+}
+
+export default App;
